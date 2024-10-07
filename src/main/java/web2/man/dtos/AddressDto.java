5678bd15f6f4d9be211e704b14f39b5fd91aebf9
Mario <mariom_mmn@hotmail.com>
2024-10-07 18:49:36 -0300

    up

--- src/main/java/web2/man/dtos/AddressDto.java
+++ src/main/java/web2/man/dtos/AddressDto.java
@@ -3,6 +3,7 @@ package web2.man.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class AddressDto {
@@ -13,11 +14,12 @@ public class AddressDto {
     @NotBlank
     private String number;
     @NotBlank
+    private String complement;
+    @NotBlank
     private String neighborhood;
     @NotBlank
     private String city;
     @NotBlank
+    @Length(min = 2, max = 2)
     private String state;
-    @NotBlank
-    private String country;
 }
